ae3015a93d2f24ddf7cea55990066c06ab20cff0
lilin <116453813@qq.com>
2026-02-27 10:54:26 +0800

    feat: 多语言默认跟随系统

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ export CC=x86_64-w64-mingw32-gcc
 export CXX=x86_64-w64-mingw32-g++
 export HOST=x86_64-w64-mingw32
 
-go build -ldflags "-s -w -H=windowsgui -extldflags=-static" -p 4 -v
+# go build -ldflags "-s -w -H=windowsgui -extldflags=-static" -p 4 -v
+go build -ldflags "-s -extldflags=-static" -p 4 -v
--- internal/lang/i18n.go
+++ internal/lang/i18n.go
@@ -5,6 +5,11 @@ package lang
 import (
 	"chisknife/asset"
 	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
 )
 
 // 存储当前语言的所有翻译键值对
@@ -19,13 +24,61 @@ func L(key string) string {
 	return key
 }
 
+// 获取系统locale
+func getLocale() (string, string) {
+	osHost := runtime.GOOS
+	defaultLang := "en"
+	defaultLoc := "US"
+	switch osHost {
+	case "windows":
+		// Exec powershell Get-Culture on Windows.
+		cmd := exec.Command("powershell", "Get-Culture | select -exp Name")
+		output, err := cmd.Output()
+		if err == nil {
+			langLocRaw := strings.TrimSpace(string(output))
+			langLoc := strings.Split(langLocRaw, "-")
+			lang := langLoc[0]
+			loc := langLoc[1]
+			return lang, loc
+		}
+	case "darwin":
+		// Exec shell Get-Culture on MacOS.
+		cmd := exec.Command("sh", "osascript -e 'user locale of (get system info)'")
+		output, err := cmd.Output()
+		if err == nil {
+			langLocRaw := strings.TrimSpace(string(output))
+			langLoc := strings.Split(langLocRaw, "_")
+			lang := langLoc[0]
+			loc := langLoc[1]
+			return lang, loc
+		}
+	case "linux":
+		envlang, ok := os.LookupEnv("LANG")
+		if ok {
+			langLocRaw := strings.TrimSpace(envlang)
+			langLocRaw = strings.Split(envlang, ".")[0]
+			langLoc := strings.Split(langLocRaw, "_")
+			lang := langLoc[0]
+			loc := langLoc[1]
+			return lang, loc
+		}
+	}
+	return defaultLang, defaultLoc
+}
+
 // 从嵌入的文件系统加载翻译数据
 // 优先加载中文翻译，失败时回退到英文
 func loadTranslations() {
-	data, err := asset.TranslationsFS.ReadFile("translations/zh-CN.json")
-	if err != nil {
+	var data []byte
+
+	if lang, loc := getLocale(); lang != "en" {
+		data, _ = asset.TranslationsFS.ReadFile(fmt.Sprintf("translations/%s-%s.json", lang, loc))
+	}
+
+	if len(data) == 0 {
 		data, _ = asset.TranslationsFS.ReadFile("translations/en.json")
 	}
+
 	json.Unmarshal(data, &translations)
 }
 
